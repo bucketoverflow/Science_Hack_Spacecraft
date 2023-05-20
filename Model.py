cd943b38956eefbc77f4bceeaa6b3996a2318353
Adrian Struzek <adrian.struzek@gmx.de>
2023-05-20 17:57:13 +0200

    Add basic Tensorboard visualization to Model.py

--- Model.py
+++ Model.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 from spacecraft import Spacecraft
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 
 
 class DQN(nn.Module):
@@ -32,7 +33,7 @@ def main():
     EPISODES = 50
     EPS_START = 0.9
     EPS_END = 0.05
-    EPS_DECAY = 200
+    EPS_DECAY = 0.01
     GAMMA = 0.99
     LR = 0.001
 
@@ -41,8 +42,14 @@ def main():
     optimizer = optim.Adam(dqn.parameters(), lr=LR)
     criterion = nn.MSELoss()
 
+    writer = SummaryWriter(
+        f'runs/spacecraft_experiment_1_{str(datetime.datetime.now()).replace(" ", "").replace(":", "_").replace(".", "_")}')
+
+    # writer.add_graph(dqn)
+    iteration = 0
+
     # Training loop
-    for episode in range(20):
+    for episode in range(EPISODES):
         state, unused = env.reset()
         print(state)
         eps = EPS_END + (EPS_START - EPS_END) * np.exp(-1. * episode / EPS_DECAY)
@@ -74,16 +81,33 @@ def main():
             # Compute loss
             loss = criterion(current_q_value, target_q_value)
 
+            if iteration % 50 == 49:  # log data every 50 iterations
+                writer.add_scalar('reward', reward, episode * iteration)
+                writer.add_scalar('loss', loss, episode * iteration)
+                prop_mass = next_state[12]
+                energy_lvl = next_state[13]
+                data_left = next_state[14]
+
+                writer.add_scalar('Propellant Mass', prop_mass, episode * iteration)
+                writer.add_scalar('Energy Level', energy_lvl, episode * iteration)
+                writer.add_scalar('Data Left', data_left, episode * iteration)
+
             # Optimize the model
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             state = next_state
+            iteration += 1
 
-    current_dir = os.getcwd()
-    time_stamp = str(datetime.datetime.now()).replace(" ", "").replace(":", "_").replace(".", "_")
-    torch.save(dqn.state_dict(), f"{current_dir}\\model_{time_stamp}.pt")
+    writer.close()
+
+    # current_dir = os.getcwd()
+    # time_stamp = str(datetime.datetime.now()).replace(" ", "").replace(":", "_").replace(".", "_")
+    # torch.save(dqn.state_dict(), f"{current_dir}\\model_{time_stamp}.pt")
+
+    # env.init_renderer(render_mode="human")
+    # env.render()
 
 
 def load_model(path, env):
@@ -95,5 +119,3 @@ def load_model(path, env):
 if __name__ == "__main__":
     main()
     print("finished!")
-
-print("finished")
