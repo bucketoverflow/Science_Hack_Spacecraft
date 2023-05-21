48a3c64d3862f5d7307604f3b2732dc5d42dea3d
Aziz ketata <mohamed.aziz.ketata@gmail.com>
2023-05-21 10:19:53 +0200

    New model

--- Model.py
+++ Model.py
@@ -7,25 +7,32 @@ from spacecraft import Spacecraft
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
-# less learning rate, much more episode
-class DQN(nn.Module):
-    # definition of the neural network , 1 hidden layer for the moment
+class Actor(nn.Module):
     def __init__(self, obs_space, action_space):
-        super(DQN, self).__init__()
+        super(Actor, self).__init__()
         self.fc1 = nn.Linear(obs_space, 128)
         self.fc2 = nn.Linear(128, 128)
         self.fc3 = nn.Linear(128, action_space)
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.to(device)
 
-    # using relu all over it
     def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return torch.tanh(self.fc3(x))
+
+class Critic(nn.Module):
+    def __init__(self, obs_space, action_space):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(obs_space + action_space, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 1)
+
+    def forward(self, x, a):
+        x = torch.cat([x, a], dim=1)
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         return self.fc3(x)
 
-
-def main(model=None):
+def main(actor=None, critic=None):
     # Initialize environment
     env = Spacecraft()
 
@@ -36,111 +43,75 @@ def main(model=None):
     EPS_DECAY = 0.01
     GAMMA = 0.99
     LR = 0.03
+    TAU = 0.01
 
-    # Initialize DQN
-    if model:
-        dqn = model
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    if actor and critic:
+        actor = actor.to(device)
+        critic = critic.to(device)
     else:
-        dqn = DQN(env.observation_space.shape[0], env.action_space.n)
-    optimizer = optim.Adam(dqn.parameters(), lr=LR)
+        actor = Actor(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
+        critic = Critic(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
+
+    target_actor = Actor(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
+    target_critic = Critic(env.observation_space.shape[0], env.action_space.shape[0]).to(device)
+
+    target_actor.load_state_dict(actor.state_dict())
+    target_critic.load_state_dict(critic.state_dict())
+
+    optimizer_actor = optim.Adam(actor.parameters(), lr=LR)
+    optimizer_critic = optim.Adam(critic.parameters(), lr=LR)
     criterion = nn.MSELoss()
 
     writer = SummaryWriter(
         f'runs/spacecraft_experiment_1_{str(datetime.datetime.now()).replace(" ", "").replace(":", "_").replace(".", "_")}')
 
-    # writer.add_graph(dqn)
     iteration = 0
 
-    # Training loop
     for episode in range(EPISODES):
-        state, unused = env.reset()
-        print(f"Episode {episode} from {EPISODES}")
-        eps = 0.05  + (EPS_START - 0.05) * np.exp (-1*episode/(EPISODES/2))
+        state, _ = env.reset()
+        state = torch.FloatTensor(state).to(device)
         done = False
-        truncated = False
 
-        writer.add_scalar('eps', eps, episode)
+        while not done:
+            actor.eval()
+            with torch.no_grad():
+                action = actor(state)
+            actor.train()
 
-        while not done and not truncated:
-            # Select action
-            if np.random.rand() < eps: # exploration
-                action = env.action_space.sample()
-            else: # exploitation
-                state_tensor = torch.FloatTensor(state)
-                q_values = dqn(state_tensor)
-                action = torch.argmax(q_values).item()
+            noise = torch.normal(mean=0., std=0.2, size=action.shape).to(device)
+            action = (action + noise).clamp(env.action_space.low, env.action_space.high)
 
-            # Take action
-            reward = 0.
-            next_state, reward, done, truncated, empty = env.step(action)
+            next_state, reward, done, _, _ = env.step(action.cpu().numpy())
+            next_state = torch.FloatTensor(next_state).to(device)
+            reward = torch.FloatTensor([reward]).to(device)
 
-            # Compute target Q value
-            next_state_tensor = torch.FloatTensor(next_state)
-            next_q_values = dqn(next_state_tensor)
-            target_q_value = reward + GAMMA * torch.max(next_q_values)
+            target_q = target_critic(next_state, target_actor(next_state))
+            target_value = reward + GAMMA * target_q
+            expected_value = critic(state, action)
 
-            # Compute current Q value
-            state_tensor = torch.FloatTensor(state)
-            current_q_value = dqn(state_tensor)[action]
+            critic_loss = criterion(expected_value, target_value.detach())
 
-            # Compute loss
-            loss = criterion(current_q_value, target_q_value)
+            optimizer_critic.zero_grad()
+            critic_loss.backward()
+            optimizer_critic.step()
 
-            if iteration % 50 == 49:  # log data every 50 iterations
-                writer.add_scalar('reward', reward, episode * iteration)
-                writer.add_scalar('loss', loss, episode * iteration)
-                prop_mass = next_state[12]
-                energy_lvl = next_state[13]
-                data_left = next_state[14]
+            actor_loss = -critic(state, actor(state)).mean()
 
-                writer.add_scalar('Propellant Mass', prop_mass, episode * iteration)
-                writer.add_scalar('Energy Level', energy_lvl, episode * iteration)
-                writer.add_scalar('Data Left', data_left, episode * iteration)
+            optimizer_actor.zero_grad()
+            actor_loss.backward()
+            optimizer_actor.step()
 
-            # Optimize the model
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            for target_param, param in zip(target_actor.parameters(), actor.parameters()):
+                target_param.data.copy_(param.data * TAU + target_param.data * (1.0 - TAU))
+
+            for target_param, param in zip(target_critic.parameters(), critic.parameters()):
+                target_param.data.copy_(param.data * TAU + target_param.data * (1.0 - TAU))
 
             state = next_state
             iteration += 1
-        current_dir = os.getcwd()
-        time_stamp = str(datetime.datetime.now()).replace(" ", "").replace(":", "_").replace(".", "_")
-        if episode == 700:
-            save_model(dqn, optimizer, episode)
-            print(reward)
-    writer.close()
-
-
-
-    # env.init_renderer(render_mode="human")
-    # env.render()
-
-    results = {"Energy used": env.en_used, "Propellent used": env.prop_used, "Reward": reward,
-               "Data Transferred": env.data_sent}
-    return results
-
-def save_model(dqn, optimizer=None, epoch=0):
-    current_dir = os.getcwd()
-    time_stamp = str(datetime.datetime.now()).replace(" ", "").replace(":", "_").replace(".", "_")
-    if optimizer:
-        torch.save(dqn.state_dict(), f"{current_dir}\\all_data_send_model_{time_stamp}.pt")
-    else:
-        state = {
-            'epoch': epoch,
-            'state_dict': dqn.state_dict(),
-            'optimizer': optimizer.state_dict()
-        }
-        torch.save(state, f"{current_dir}\\all_data_send_model_{time_stamp}.pt")
-
-def load_model(path, env):
-    model = DQN(env.observation_space.shape[0], env.action_space.n)
-    model.load_state_dict(torch.load(path))
-
 
 if __name__ == "__main__":
-    env = Spacecraft()
-    model = load_model(
-    "/Users/benedikt/Desktop/environment/Klon\\all_data_send_model_2023-05-2101_20_53_497069.pt", env)
-    Result = main(model)
-    print("Finished")
+    main()
+
